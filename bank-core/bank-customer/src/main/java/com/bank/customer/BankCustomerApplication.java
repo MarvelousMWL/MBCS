84a8be53693f37f88b328e7802bf63804b432ca8
meiwenlong <2026232892@qq.com>
2026-06-22 11:35:27 +0800

    fix: 移除模块级重复MapperScan，消除17个Mapper Bean重复定义警告

--- bank-core/bank-customer/src/main/java/com/bank/customer/BankCustomerApplication.java
+++ bank-core/bank-customer/src/main/java/com/bank/customer/BankCustomerApplication.java
@@ -1,13 +1,11 @@
 package com.bank.customer;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.bank.customer", "com.bank.common"})
-@MapperScan(basePackages = {"com.bank.customer.infrastructure.persistence", "com.bank.common"})
 public class BankCustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankCustomerApplication.class, args);
